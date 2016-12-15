67b48fcb3a49e26d23da32cdbd070916c8cdfaea
Antonio SJ Musumeci <trapexit@spawn.link>
2016-12-15 17:11:39 -0500

    compute inode in readdir

---
+++ src/fs_devid.hpp
@@ -0,0 +1,45 @@
+/*
+  ISC License
+
+  Copyright (c) 2016, Antonio SJ Musumeci <trapexit@spawn.link>
+
+  Permission to use, copy, modify, and/or distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#ifndef __FS_DEVID_HPP__
+#define __FS_DEVID_HPP__
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace fs
+{
+  static
+  inline
+  dev_t
+  devid(const int fd)
+  {
+    int rv;
+    struct stat st;
+
+    rv = ::fstat(fd,&st);
+    if(rv == -1)
+      return -1;
+
+    return st.st_dev;
+  }
+}
+
+#endif
+
