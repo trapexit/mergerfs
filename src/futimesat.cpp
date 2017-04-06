cf567f11ce73a5bba646967bc9d47373d8d9c3fa
Antonio SJ Musumeci <trapexit@spawn.link>
2017-04-06 17:06:34 -0400

    remove unnecessary futimesat abstraction

--- src/futimesat.cpp
+++
@@ -1,5 +0,0 @@
-#include "futimesat.hpp"
-
-#ifdef __APPLE__
-#include "futimesat_osx.icpp"
-#endif
--- src/futimesat.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __UTIMESAT_HPP__
-#define __UTIMESAT_HPP__
-
-#if __APPLE__
-int
-_futimesat(int dirfd, const char* path, struct timeval *tvp);
-#endif
-
-#endif
