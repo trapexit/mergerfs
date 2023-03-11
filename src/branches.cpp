d04e04e89a08e577387ec5489b7756f768cc63df
Ilya Taranov <ilya@taranoff.net>
2023-03-10 20:23:55 -0800

    Fix config file parsing

--- src/branches.cpp
+++ src/branches.cpp
@@ -80,11 +80,14 @@ namespace l
     uint64_t offset;
 
     offset = s_.find_first_not_of("+<>-=");
+    if (offset == std::string::npos) {
+      return;
+    }
+
     if(offset > 1)
       offset = 2;
     *instr_ = s_.substr(0,offset);
-    if(offset != std::string::npos)
-      *values_ = s_.substr(offset);
+    *values_ = s_.substr(offset);
   }
 
   static
