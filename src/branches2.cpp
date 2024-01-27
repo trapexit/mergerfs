b78d01a62986f17d28349d9628e36a77f8bba8f9
Antonio SJ Musumeci <trapexit@spawn.link>
2024-01-27 00:56:08 -0600

    branches2.cpp

--- src/branches2.cpp
+++ src/branches2.cpp
@@ -19,6 +19,7 @@ Branches2::Branches2(toml::value const &v_)
         {
           auto const &branch_table = branch.as_table();
 
+          if(branch_table["type"].as_string() == "literal")
         }
     }
 }
