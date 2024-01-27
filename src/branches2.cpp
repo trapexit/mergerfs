7b76221d1bdcd476af9abf157cda9aa148d2ec8c
Antonio SJ Musumeci <trapexit@spawn.link>
2024-01-27 00:49:38 -0600

    branches2.cpp

--- src/branches2.cpp
+++ src/branches2.cpp
@@ -9,7 +9,7 @@ Branches2::Branches2()
 
 Branches2::Branches2(toml::value const &v_)
 {
-  auto const &branches = toml::find(v_,"branch");
+  auto const &tiers = toml::find(v_,"tier");
 
   for(auto const &branch : branches.as_array())
     {
