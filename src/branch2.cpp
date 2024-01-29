ebe35f5a0b51f4f4ffa5be2b60f0ac958fb22892
Antonio SJ Musumeci <trapexit@spawn.link>
2024-01-29 00:00:38 -0600

    branch2.cpp

--- src/branch2.cpp
+++ src/branch2.cpp
@@ -1,7 +1,14 @@
 #include "branch2.hpp"
 
 
+Branch2::Branch2()
+  : mode(Mode::RW)
+{
+
+}
+
 Branch2::Branch2(toml::value const &v_)
 {
   enabled = v_.at("enabled").as_boolean();
+  mode    = v_.at("mode").as_string()
 }
