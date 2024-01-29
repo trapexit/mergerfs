03d90a3899004afe300843b85d60f100c156cb86
Antonio SJ Musumeci <trapexit@spawn.link>
2024-01-29 00:07:09 -0600

    branch2.cpp

--- src/branch2.cpp
+++ src/branch2.cpp
@@ -10,6 +10,8 @@ Branch2::Branch2()
 Branch2::Branch2(toml::value const &v_)
   : mode(Mode::RW)
 {
-  enabled = v_.at("enabled").as_boolean();
-  mode    = Mode::_from_string(toml::find<std::string>(v_,"mode").c_str());
+  enabled        = v_.at("enabled").as_boolean();
+  mode           = Mode::_from_string(toml::find<std::string>(v_,"mode").c_str());
+  min_free_space = 0;
+  path           = v_.at("path").as_string();
 }
