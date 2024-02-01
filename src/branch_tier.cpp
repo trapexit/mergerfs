dc5d544f1b4bc3a8ce44fdb24f6fc1195d5ffbf6
Antonio SJ Musumeci <trapexit@spawn.link>
2024-01-31 23:30:21 -0600

    branch_tier.cpp

--- src/branch_tier.cpp
+++ src/branch_tier.cpp
@@ -27,6 +27,7 @@ namespace l
     fs::glob(pattern,&paths);
 
     auto table = v_;
+    //    branches_.resize(branches_.size() + paths.size());
     for(auto &path : paths)
       {
         table["path"] = path;
