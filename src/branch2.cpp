18635d489abbbc24313fa32842905f47101440d0
Antonio SJ Musumeci <trapexit@spawn.link>
2024-01-31 21:17:45 -0600

    branch2.cpp

--- src/branch2.cpp
+++ src/branch2.cpp
@@ -1,5 +1,7 @@
 #include "branch2.hpp"
 
+#include <iostream>
+
 
 Branch2::Branch2()
   : mode(Mode::RW)
@@ -10,9 +12,11 @@ Branch2::Branch2()
 Branch2::Branch2(toml::value const &v_)
   : mode(Mode::RW)
 {
+  std::cout << v_ << '\n';
+  
   enabled        = v_.at("enabled").as_boolean();
   mode           = Mode::_from_string(toml::find<std::string>(v_,"mode").c_str());
   min_free_space = 0;
   path           = toml::find<std::string>(v_,"path");
-  printf("%s\n",path.string().c_str());
+
 }
