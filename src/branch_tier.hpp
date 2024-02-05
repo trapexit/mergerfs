c5d7a4d4e6365df0ded2c8f3000ac5b300ec8271
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-04 22:26:25 -0600

    branch_tier.hpp

--- src/branch_tier.hpp
+++ src/branch_tier.hpp
@@ -19,8 +19,8 @@ public:
   std::vector<Branch2>::iterator begin() { return _branches.begin(); }
   std::vector<Branch2>::iterator end() { return _branches.end(); }
 
-  std::vector<Branch2>::iterator begin() const { return _branches.begin(); }
-  std::vector<Branch2>::iterator end() const { return _branches.end(); }
+  std::vector<Branch2>::const_iterator begin() const { return _branches.begin(); }
+  std::vector<Branch2>::const_iterator end() const { return _branches.end(); }
   
 
 private:
