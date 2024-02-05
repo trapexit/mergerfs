b22991b8781ae41dce148d8e262a85437f390507
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-04 22:23:35 -0600

    branches2.cpp

--- src/branches2.cpp
+++ src/branches2.cpp
@@ -23,7 +23,19 @@ int
 Branches2::clonepath(ghc::filesystem::path basepath_,
                      ghc::filesystem::path relpath_)
 {
+  int rv;
   
+  for(auto const &tier : _branches)
+    {
+      for(auto const &branch : tier)
+        {
+          if(branch.path == basepath_)
+            continue;
+          rv = fs::clonepath(branch.path,basepath_,relpath_);
+          if(rv == 0)
+            return 0;
+        }
+    }
 
   return 0;
 }
