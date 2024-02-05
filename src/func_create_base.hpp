47be2da7a97e08700cef985f20e280b2cb3846a4
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-05 11:47:58 -0600

    func_create_base.hpp

--- src/func_create_base.hpp
+++ src/func_create_base.hpp
@@ -35,9 +35,9 @@ namespace Func
     virtual ~CreateBase() {};
 
   public:
-    virtual int operator()(Branches2      &branches,
-                           fs::Path const &fusepath,
-                           mode_t const    mode,
-                           fuse_file_info_t            *ffi) = 0;
+    virtual int operator()(Branches2        &branches,
+                           fs::Path const   &fusepath,
+                           mode_t const      mode,
+                           fuse_file_info_t *ffi)
   };
 }
