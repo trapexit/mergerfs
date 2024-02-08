2d6fe4711ef8112df5675b1242be611effb20656
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-07 20:18:23 -0600

    branch2.cpp

--- src/branch2.cpp
+++ src/branch2.cpp
@@ -18,6 +18,16 @@ Branch2::Branch2()
 
 }
 
+Branch2::Branch2(Branch2 &v_)
+  : enabled(v_.enabled),
+    mode(v_.mode),
+    min_free_space(v_.min_free_space),
+    fd(v_.fd),
+    path(v_.path)
+{
+  v_.fd = -1;
+}
+
 Branch2::Branch2(Branch2 &&v_)
   : enabled(v_.enabled),
     mode(v_.mode),
