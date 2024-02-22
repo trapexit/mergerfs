170630f272a541ca9c203a5c202461ba4ff591a1
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-21 20:08:56 -0600

    fuse_getattr.cpp

--- src/fuse_getattr.cpp
+++ src/fuse_getattr.cpp
@@ -143,7 +143,7 @@ namespace l
 
     for(int c = 0; c < 2; c++)
       {
-        std::string *basepath = cache.find(fusepath_);
+        std::stringc const *basepath = cache.find(fusepath_);
         if(i == NULL)
           {
             rv = searchFunc_(branches_,fusepath_,&basepaths);
