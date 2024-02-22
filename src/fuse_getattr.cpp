e543d3284db64cf467de42a67a4f82ce5ca3843e
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-21 20:09:21 -0600

    fuse_getattr.cpp

--- src/fuse_getattr.cpp
+++ src/fuse_getattr.cpp
@@ -155,7 +155,7 @@ namespace l
             i = cache.insert(fusepath_,basepaths[0]);
           }
 
-        fullpath = fs::path::make(*i,fusepath_);
+        fullpath = fs::path::make(basepath,fusepath_);
 
         rv = l::getattr(fullpath,followsymlinks_,st_);
         if(rv == 0)
