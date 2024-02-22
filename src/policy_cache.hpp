c395ff19a9d7987dcf999249ea8ae7c28e90eca1
Antonio SJ Musumeci <trapexit@spawn.link>
2024-02-22 00:14:07 -0600

    policy_cache.hpp

--- src/policy_cache.hpp
+++ src/policy_cache.hpp
@@ -52,7 +52,7 @@ public:
     nonstd::optional<std::string> rv;
 
     auto func =
-      [&](const auto &v_)
+      [&](const nonstd::optional<std::string> &v_)
       {
         rv = v_;
       };
