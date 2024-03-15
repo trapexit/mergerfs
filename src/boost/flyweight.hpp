2c0b03886df2f66a386ca36e5df16acc78a25ccf
Antonio SJ Musumeci <trapexit@spawn.link>
2024-03-15 07:25:31 -0500

    policy_cache.hpp

--- src/boost/flyweight.hpp
+++ src/boost/flyweight.hpp
@@ -6,21 +6,17 @@
  * See http://www.boost.org/libs/flyweight for library home page.
  */
 
-#ifndef BOOST_FLYWEIGHT_NO_LOCKING_FWD_HPP
-#define BOOST_FLYWEIGHT_NO_LOCKING_FWD_HPP
+#ifndef BOOST_FLYWEIGHT_HPP
+#define BOOST_FLYWEIGHT_HPP
 
 #if defined(_MSC_VER)
 #pragma once
 #endif
 
-namespace boost{
-
-namespace flyweights{
-
-struct no_locking;
-
-} /* namespace flyweights */
-
-} /* namespace boost */
+#include <boost/flyweight/flyweight.hpp>
+#include <boost/flyweight/hashed_factory.hpp>
+#include <boost/flyweight/refcounted.hpp>
+#include <boost/flyweight/simple_locking.hpp>
+#include <boost/flyweight/static_holder.hpp>
 
 #endif
