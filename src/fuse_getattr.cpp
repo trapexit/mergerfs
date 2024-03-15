b034f8733820995fa3e2f9cf13bcffe7358082f1
Antonio SJ Musumeci <trapexit@spawn.link>
2024-03-15 07:34:41 -0500

    fuse_getattr.cpp

--- src/fuse_getattr.cpp
+++ src/fuse_getattr.cpp
@@ -139,7 +139,7 @@ namespace l
     int rv;
     std::string fullpath;
     StrVec basepaths;
-    const char *basepath;
+    PolicyCache::Key basepath;
     static PolicyCache cache(1024*1024);
 
     basepath = cache.find(fusepath_);
