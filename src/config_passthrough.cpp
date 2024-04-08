45dbec907bac8f5fe653830a8bbf7886d926f628
Antonio SJ Musumeci <trapexit@spawn.link>
2024-04-08 12:59:15 -0500

    config_passthrough.cpp

--- src/config_passthrough.cpp
+++ src/config_passthrough.cpp
@@ -31,6 +31,7 @@ template<>
 int
 Passthrough::from_string(const std::string &s_)
 {
+  _data = (int)BE_Passthrough::_from_string(s_);
   if(s_ == "off")
     _data = Passthrough::ENUM::OFF;
   ef(s_ == "ro")
