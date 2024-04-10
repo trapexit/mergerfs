78d754df6b46f033773014e823764e566040dab7
Antonio SJ Musumeci <trapexit@spawn.link>
2024-04-09 23:21:20 -0500

    fuse_lowlevel.c

--- libfuse/lib/fuse_lowlevel.c
+++ libfuse/lib/fuse_lowlevel.c
@@ -282,11 +282,6 @@ fill_open(struct fuse_open_out   *arg_,
       arg_->open_flags |= FOPEN_PASSTHROUGH;
       arg_->backing_id  = ffi_->backing_id;
     }
-
-  fprintf(stderr,
-          "%d %d\n",
-          !!(arg_->open_flags | FOPEN_KEEP_CACHE),
-          !!(arg_->open_flags | FOPEN_PASSTHROUGH));
 }
 
 int
