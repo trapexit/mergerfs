dda90e9646c3d6e4e3479ff45dbfd8142dc55b71
Antonio SJ Musumeci <trapexit@spawn.link>
2024-04-11 21:37:49 -0500

    fuse_open.cpp

--- src/fuse_open.cpp
+++ src/fuse_open.cpp
@@ -233,9 +233,9 @@ namespace l
     fi = reinterpret_cast<FileInfo*>(ffi_->fh);
 
     foo.visit(fi->fusepath,
-              [](int backing_id_)
+              [&](int backing_id_)
               {
-                
+                backing_id = backing_id_;
               });
     
     backing_id = fuse_passthrough_open(fc_,fi->fd);
