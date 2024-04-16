767eef2ba3bfa376656b700796532853bf74aee3
Antonio SJ Musumeci <trapexit@spawn.link>
2024-04-15 22:46:19 -0500

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -2690,7 +2690,7 @@ fuse_lib_setxattr(fuse_req_t             req,
   struct fuse *f;
   struct fuse_setxattr_in *arg;
 
-  arg   = fuse_hdr_arg(hdr_);
+  arg   = (fuse_setxattr_in*)fuse_hdr_arg(hdr_);
   if((req->f->conn.capable & FUSE_SETXATTR_EXT) && (req->f->conn.want & FUSE_SETXATTR_EXT))
     name = PARAM(arg);
   else
