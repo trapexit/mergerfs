30b7cbb13f4eee7a83ec17857cce79789e69415b
Antonio SJ Musumeci <trapexit@spawn.link>
2024-12-02 07:46:58 -0600

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -106,13 +106,6 @@ struct node_table
   size_t   split;
 };
 
-#define container_of(ptr,type,member) ({                        \
-      const typeof( ((type *)0)->member ) *__mptr = (ptr);      \
-      (type *)( (char *)__mptr - offsetof(type,member) );})
-
-#define list_entry(ptr,type,member)             \
-  container_of(ptr,type,member)
-
 struct list_head
 {
   struct list_head *next;
