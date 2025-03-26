a98dc796c3d6d4a87b484419a7bae29a1fdf7a86
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-25 22:11:58 -0500

    fuse_init.cpp

--- src/fuse_init.cpp
+++ src/fuse_init.cpp
@@ -87,7 +87,6 @@ namespace l
       {
         f.seekp(0);
         f << (uint64_t)cfg_->fuse_msg_size;
-        f.flush();
         syslog_info("/proc/sys/fs/fuse/max_pages_limit changed to %d",
                     (uint64_t)cfg_->fuse_msg_size);
       }
