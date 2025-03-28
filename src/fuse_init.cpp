3b8c3ac205d44289bfdd218505f7cbc474faa873
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-27 23:31:33 -0500

    fuse_init.cpp

--- src/fuse_init.cpp
+++ src/fuse_init.cpp
@@ -120,7 +120,7 @@ namespace l
       {
         l::want(conn_,FUSE_CAP_MAX_PAGES);
         conn_->max_pages = cfg_->fuse_msg_size;
-        syslog_info("requesting max pages size of %d",
+        syslog_info("requesting max pages size of %z",
                     (uint64_t)cfg_->fuse_msg_size);
       }
     else
