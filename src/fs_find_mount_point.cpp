1b1e7a1b5259b6285d656c6db27b3be1b72e2ca0
Antonio SJ Musumeci <trapexit@spawn.link>
2025-04-16 11:42:11 -0500

    fs_find_mount_point.cpp

--- src/fs_find_mount_point.cpp
+++ src/fs_find_mount_point.cpp
@@ -20,7 +20,7 @@ fs::find_mount_point(const ghc::filesystem::path &path_)
   
   rv = fs::lstat(can_path,&initial_st);
   if(rv == -1)
-    return std::error_code(-errno);
+    return std::error_code(-errno,std::system_category());
 
   tmp_path = can_path;
   while(tmp_path != tmp_path.root_path())
