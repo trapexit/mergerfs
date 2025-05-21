03c494d0752b20bcd199dcbcfe8c6cc1b541fcba
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-20 22:58:11 -0500

    mergerfs.cpp

--- src/mergerfs.cpp
+++ src/mergerfs.cpp
@@ -351,7 +351,7 @@ main(int    argc_,
     map.visit(0,
               [](auto &pair_)
               {
-                fmt::print("thread0: sleeping 1s\n");
+                fmt::print("thread0: sleeping 5s\n");
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 pair_.second = 69;
                 fmt::print("thread0: {}={}\n",pair_.first,pair_.second);
