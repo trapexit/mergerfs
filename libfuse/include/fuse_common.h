f59bf1153a49bc705a4b7ce3fd9b146f726d5120
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-21 08:16:19 -0500

    fuse_common.h

--- libfuse/include/fuse_common.h
+++ libfuse/include/fuse_common.h
@@ -87,6 +87,7 @@ struct fuse_file_info_t
   uint32_t parallel_direct_writes:1;
 
   uint32_t noflush:1;
+  uint32_t passthrough:1;
 
   /** File handle.  May be filled in by filesystem in open().
       Available in all other file operations */
