1f6eda3894b03f01f6e585e8e6bb348804123f93
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-21 10:31:11 -0500

    fuse_release.cpp

--- src/fuse_release.cpp
+++ src/fuse_release.cpp
@@ -49,7 +49,7 @@ namespace l
                                {
                                  val.second.ref_count--;
                                  fmt::print("release {}; ref: {}\n",
-                                            val.first,
+                                            val.first.string(),
                                             val.second.ref_count);
                                  return (val.second.ref_count == 0);
                                });
