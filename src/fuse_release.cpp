a62fdbf38c54669c1d9032dab6181e85cd7f8271
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-22 17:38:59 -0500

    fuse_release.cpp

--- src/fuse_release.cpp
+++ src/fuse_release.cpp
@@ -38,12 +38,12 @@ _erase_if_lambda(FileInfo *fi_)
       val.second.ref_count--;
       if(val.second.ref_count <= 0)
         {
-          fs::close(fi_->fd);
 
           const ugid::SetRootGuard ugid;
           const fuse_context *fc = fuse_get_context();
           int rv = fuse_passthrough_close(fc,val.second.backing_id);
           fmt::println("fuse_passthrough_close() = {} {} {}",rv,errno,strerror(errno));
+          fs::close(fi_->fd);
         }
       return (val.second.ref_count == 0);
     };
