2a6186f56b08f96c34274dad698c443a56c4f5b9
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-21 20:46:53 -0500

    fs_inode.hpp

--- src/fs_inode.hpp
+++ src/fs_inode.hpp
@@ -35,16 +35,16 @@ namespace fs
     int set_algo(const std::string &s);
     std::string get_algo(void);
 
-    uint64_t calc(const nonstd::string_view basepath,
-                  const nonstd::string_view fusepath,
+    uint64_t calc(const std::string_view basepath,
+                  const std::string_view fusepath,
                   const mode_t              mode,
                   const ino_t               ino);
 
-    void calc(const nonstd::string_view  basepath,
-              const nonstd::string_view  fusepath,
-              struct stat               *st);
-    void calc(const nonstd::string_view  basepath,
-              const nonstd::string_view  fusepath,
-              struct fuse_statx         *st);
+    void calc(const std::string_view  basepath,
+              const std::string_view  fusepath,
+              struct stat            *st);
+    void calc(const std::string_view  basepath,
+              const std::string_view  fusepath,
+              struct fuse_statx      *st);
   }
 }
