1298594e5dedf44d66b39617adb5ba4f949e337f
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-22 14:35:12 -0500

    fuse_open.cpp

--- src/fuse_open.cpp
+++ src/fuse_open.cpp
@@ -349,6 +349,7 @@ _open_passthrough_first_lambda(const char       *fusepath_,
           FileInfo *fi;
 
           fi = reinterpret_cast<FileInfo*>(ffi_->fh);
+          val.second.fd         = fi->fd;
           val.second.backing_id = ffi_->backing_id;
           val.second.branch     = fi->branch;
         }
