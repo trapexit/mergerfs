460cf1a33889636253b600503d41d99e6cb6db85
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-22 23:54:56 -0500

    checkpoint

---
+++ src/fuse_passthrough.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ugid.hpp"
+
+#include "fuse.h"
+
+namespace FUSE
+{
+  static
+  int
+  passthrough_open(const fuse_context *fc_,
+                   const int           fd_)
+  {
+    const ugid::SetRootGuard _;
+
+    return fuse_passthrough_open(fc_,fd_);
+  }
+
+  static
+  int
+  passthrough_close(const fuse_context *fc_,
+                    const int           backing_id_)
+  {
+    const ugid::SetRootGuard _;
+
+    return fuse_passthrough_open(fc_,backing_id_);
+  }
+}
