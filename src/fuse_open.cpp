0b03ca5a6c70b5d9807a84ae901896106ef31df5
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-23 10:36:00 -0500

    fuse_open.cpp

--- src/fuse_open.cpp
+++ src/fuse_open.cpp
@@ -298,7 +298,7 @@ _open_for_lambda(const fuse_context *fc_,
   if(backing_id < 0)
     return 0;
 
-  pd_->backing_id    = backing_id;
+  pd_->backing_id   = backing_id;
   ffi_->backing_id  = backing_id;
   ffi_->passthrough = true;
   ffi_->keep_cache  = false;
