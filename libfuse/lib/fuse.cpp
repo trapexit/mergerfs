34db5f9acb9d729a1931d2159ad89ed9f3696751
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-23 23:48:02 -0500

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -1489,12 +1489,14 @@ req_fuse_prepare(fuse_req_t req)
 {
   struct fuse_context_i *c = fuse_get_context_internal();
   const struct fuse_ctx *ctx = fuse_req_ctx(req);
+
   c->req = req;
   c->ctx.fuse = req_fuse(req);
   c->ctx.uid = ctx->uid;
   c->ctx.gid = ctx->gid;
   c->ctx.pid = ctx->pid;
   c->ctx.umask = ctx->umask;
+
   return c->ctx.fuse;
 }
 
