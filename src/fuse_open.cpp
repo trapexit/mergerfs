8b78a7aa7f9e4d098e2c2e293b7dd805d76d6d1d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-24 17:27:52 -0500

    fuse_open.cpp

--- src/fuse_open.cpp
+++ src/fuse_open.cpp
@@ -395,9 +395,6 @@ _open_passthrough_update_lambda(const fuse_context *fc_,
                                         fusepath_,
                                         ffi_,
                                         &val_.second);
-      fmt::print("open passthrough update: nodeid={}; backing_id={};\n",
-                 val_.first,
-                 val_.second.backing_id);
     };
 }
 
