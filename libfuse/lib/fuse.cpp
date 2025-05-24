747c0bdc314c36aefdf4b7dbd0b59456c1bd7b74
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-23 22:20:01 -0500

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -2074,6 +2074,8 @@ fuse_lib_unlink(fuse_req_t             req,
   f = req_fuse_prepare(req);
   err = get_path_wrlock(f,hdr_->nodeid,name,&path,&wnode);
 
+  pthread_mutex_timedlock(&f->lock,{1,0});
+
   if(!err)
     {
       pthread_mutex_lock(&f->lock);
