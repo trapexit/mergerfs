4789d152339d8d3d5221435a9681c12b5ba26b80
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-24 16:57:51 -0500

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -2302,9 +2302,9 @@ fuse_lib_create(fuse_req_t             req,
   f = req_fuse_prepare(req);
 
   // opportunistically allocate a node so we have a new nodeid
-  new_node = find_node(f,hdr_->nodeid,name);
-  nodeid = node->nodeid;
-    fprintf(stderr,"fuse.cpp: create: nodeid=%zu\n",node->nodeid);
+  {
+    node_t *new_node = find_node(f,hdr_->nodeid,name);
+    nodeid = node->nodeid;
   }
 
   err = get_path_name(f,hdr_->nodeid,name,&path);
