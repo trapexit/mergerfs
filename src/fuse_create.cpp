8c9e4d31064bc8b3f1372e5e5d6b75601528b62d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-23 21:11:52 -0500

    fuse_create.cpp

--- src/fuse_create.cpp
+++ src/fuse_create.cpp
@@ -217,7 +217,18 @@ _create_for_insert_lambda(const fuse_context *fc_,
                           fuse_file_info_t   *ffi_,
                           PassthroughDetails &pd_)
 {
+  return 0;
+}
 
+static
+int
+_create_for_update_lambda(const fuse_context *fc_,
+                          const char         *fusepath_,
+                          const mode_t        mode_,
+                          fuse_file_info_t   *ffi_,
+                          PassthroughDetails &pd_)
+{
+  return 0;
 }
 
 static
