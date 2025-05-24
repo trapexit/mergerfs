593542a8cbba83523b618b8de0a99f6981c53276
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-24 00:57:59 -0500

    fuse_lowlevel.cpp

--- libfuse/lib/fuse_lowlevel.cpp
+++ libfuse/lib/fuse_lowlevel.cpp
@@ -1962,14 +1962,14 @@ fuse_ll_buf_process_read(struct fuse_session *se_,
   if(req == NULL)
     return fuse_send_enomem(se_->f,se_->ch,in->unique);
 
-  req->unique  = in->unique;
+  req->unique     = in->unique;
   req->ctx.unique = in->unique;
   req->ctx.nodeid = in->nodeid;
   req->ctx.opcode = in->opcode;
-  req->ctx.uid = in->uid;
-  req->ctx.gid = in->gid;
-  req->ctx.pid = in->pid;
-  req->ch      = se_->ch;
+  req->ctx.uid    = in->uid;
+  req->ctx.gid    = in->gid;
+  req->ctx.pid    = in->pid;
+  req->ch         = se_->ch;
 
   err = ENOSYS;
   if(in->opcode >= FUSE_MAXOPS)
