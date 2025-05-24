98c3f91c21e41449692d0d5af40e449acfd07306
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-24 16:46:18 -0500

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -2301,7 +2301,9 @@ fuse_lib_create(fuse_req_t             req,
 
   //TODO: REMOVE?
   node_t *node;
+  uint64_t nodeid;
   node = find_node(f,hdr_->nodeid,name);
+  nodeid = node->nodeid;
   fprintf(stderr,"fuse.cpp: create: nodeid=%zu\n",node->nodeid);
 
   err = get_path_name(f,hdr_->nodeid,name,&path);
