b28aa63a2de87dd7324a774062851f67f71ca4f9
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-30 23:36:07 -0500

    checkpoint

---
+++ src/fs_open_fd.cpp
@@ -0,0 +1,32 @@
+#include "fs_open_fd.hpp"
+
+#if defined(__linux__)
+#include "fmt/core.h"
+#include "fs_openat.hpp"
+#include "procfs.hpp"
+
+int
+fs::open_fd(const int fd_,
+            const int flags_)
+{
+  return fs::openat(procfs::PROC_SELF_FD_FD,
+                    fmt::format("{}",fd_),
+                    flags_);
+}
+#elif defined(__FreeBSD__)
+#include "fs_openat.hpp"
+
+int
+fs::open_fd(const int fd_,
+            const int flags_)
+{
+  return fs::openat(fd_,"",flags_|O_EMPTY_PATH);
+}
+#else
+int
+fs::open_fd(const int fd_,
+            const int flags_)
+{
+  return -ENOSUP;
+}
+#endif
