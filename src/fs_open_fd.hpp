9f47e2e26c760fdd448be43b72541a2066e3f4cd
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-30 22:46:51 -0500

    fs_open_fd.hpp

--- src/fs_open_fd.hpp
+++ src/fs_open_fd.hpp
@@ -3,25 +3,5 @@
 
 namespace fs
 {
-#ifdef __linux__
-  static
-  inline
-  int
-  open_fd(const int fd_,
-          const int flags_)
-  {
-    return 0;
-  }
-#elif defined __FreeBSD__
-  static
-  inline
-  int
-  open_fd(const int fd_,
-          const int flags_)
-  {
-    return 0;
-  }
-#else
-#error "Not supported"  
-#endif
+  int open_fd(const int fd, const int flags);
 }
