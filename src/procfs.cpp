11019781a43499ee6502ba6751206d8f007942b9
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-30 23:19:56 -0500

    procfs.cpp

--- src/procfs.cpp
+++ src/procfs.cpp
@@ -18,7 +18,7 @@
 #include <pthread.h>
 
 static int g_PROCFS_DIR_FD = -1;
-int g_PROC_SELF_FD_FD = -1;
+int PROC_SELF_FD_FD = -1;
 static constexpr const char PROCFS_PATH[] = "/proc";
 static constexpr const char PROC_SELF_FD[] = "/proc/self/fd";
 
