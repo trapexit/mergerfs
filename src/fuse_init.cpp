5b6486ad4524cb34a5eebc6233112bd7a95f76e9
Antonio SJ Musumeci <trapexit@spawn.link>
2025-05-30 23:13:32 -0500

    fuse_init.cpp

--- src/fuse_init.cpp
+++ src/fuse_init.cpp
@@ -218,14 +218,6 @@ namespace FUSE
 
     l::spawn_thread_to_set_readahead();
 
-    state.proc_self_fd_fd = open("/proc/self/fd",O_RDWR|O_PATH|O_DIRECTORY);
-    if(state.proc_self_fd_fd == -1)
-      state.proc_self_fd_fd = open("/dev/fd",O_RDWR|O_PATH|O_DIRECTORY);
-    assert(state.proc_self_fd_fd != -1);
-
-    fmt::print("fd = {}\n",state.proc_self_fd_fd);
-    system("ls -lh /dev/fd");
-
     return NULL;
   }
 }
