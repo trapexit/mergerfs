c744de81cb391fe051b5bdb6410cf14a716e8b25
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-10 23:49:26 -0500

    func_getattr_combine.cpp

--- src/func_getattr_combine.cpp
+++ src/func_getattr_combine.cpp
@@ -4,6 +4,12 @@
 #include "fs_inode.hpp"
 #include "timespec_utils.hpp"
 
+std::string_view
+Func2::GetAttrCombine::name() const
+{
+  return "";
+}
+
 int
 Func2::GetAttrCombine::operator()(const Branches  &branches_,
                                   const fs::Path  &fusepath_,
