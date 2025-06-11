72ec0a5fb463c83b01a320bb0601e8b48794ca59
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-10 23:59:47 -0500

    func_getattr_combine.hpp

--- src/func_getattr_combine.hpp
+++ src/func_getattr_combine.hpp
@@ -9,7 +9,7 @@ namespace Func2
     ~GetAttrCombine() {}
 
   public:
-    const std::string& name() const;
+    std::string_view name() const;
 
   public:
     int operator()(const Branches  &branches,
