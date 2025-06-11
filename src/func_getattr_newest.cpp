457e9412186e6142346bc3a4b772712222efc763
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-11 00:01:23 -0500

    func_getattr_newest.cpp

--- src/func_getattr_newest.cpp
+++ src/func_getattr_newest.cpp
@@ -4,6 +4,12 @@
 #include "fs_inode.hpp"
 #include "timespec_utils.hpp"
 
+std::string_view
+Func2::GetAttrNewest::name() const
+{
+  return "newest";
+}
+
 int
 Func2::GetAttrNewest::operator()(const Branches  &branches_,
                                  const fs::Path  &fusepath_,
