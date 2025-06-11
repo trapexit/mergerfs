7292a816d6bf063135bf8f13e96e061c7c4268dd
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-10 22:21:19 -0500

    checkpoint

---
+++ src/func_getattr.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "func_getattr_base.hpp"
+#include "func_getattr_combine.hpp"
+
+#include "fmt/core.h"
+
+#include <memory>
+
+namespace Func2
+{
+  class GetAttr
+  {
+  public:
+    GetAttr()
+    {
+      _impl = std::make_shared<Func2::GetAttrCombine>();
+    }
+
+  public:
+    int
+    operator()(const Branches  &branches_,
+               const fs::Path  &fusepath_,
+               struct stat     *st_,
+               fuse_timeouts_t *timeout_)
+    {
+      std::lock_guard<std::mutex> guard{_lock};
+
+      return (*_impl)(branches_,fusepath_,st_,timeout_);
+    }
+
+  private:
+    std::mutex _lock;
+    std::shared_ptr<Func2::GetAttrBase> _impl;
+  };
+}
