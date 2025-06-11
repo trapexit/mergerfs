272e85409bf386141e4ccc00bba261ad47918535
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-10 23:47:40 -0500

    func_getattr_newest.hpp

--- src/func_getattr_newest.hpp
+++ src/func_getattr_newest.hpp
@@ -9,6 +9,9 @@ namespace Func2
     ~GetAttrNewest() {}
 
   public:
+    std::string name() const;
+
+  public:
     int operator()(const Branches  &branches,
                    const fs::Path  &fusepath,
                    struct stat     *st,
