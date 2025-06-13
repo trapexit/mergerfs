989edfdc430158bb4b5081a697eed37c7593a3c8
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-12 23:39:56 -0500

    symlinkify.hpp

--- src/symlinkify.hpp
+++ src/symlinkify.hpp
@@ -89,7 +89,7 @@ namespace symlinkify
                             fuse_statx        *st_,
                             const time_t       timeout_)
   {
-    if(symlinkify::can_be_symlink(st_,timeout_))
+    if(symlinkify::can_be_symlink(*st_,timeout_))
       symlinkify::convert(target_,st_);
   }
 }
