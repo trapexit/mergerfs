d7c92361cdbb3feef691c046f5a723ae9180a19d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-12 23:55:44 -0500

    func_getattr_base.hpp

--- src/func_getattr_base.hpp
+++ src/func_getattr_base.hpp
@@ -29,6 +29,6 @@ namespace Func2
                            const fs::Path &fusepath,
                            struct stat    *st,
                            const FollowSymlinksEnum follow_symlinks,
-                           const uint64_t symlinkify_timeout) = 0;
+                           const time_t symlinkify_timeout) = 0;
   };
 }
