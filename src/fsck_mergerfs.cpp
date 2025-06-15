9e293d2103a263a1da33bc9bc27f7182f6733f4b
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-14 20:35:35 -0500

    fsck_mergerfs.cpp

--- src/fsck_mergerfs.cpp
+++ src/fsck_mergerfs.cpp
@@ -44,6 +44,8 @@ _compare_files(const std::string              &path_,
   struct stat st;
   std::vector<struct stat> stats;
 
+  fmt::print("{}\n",path_);
+
   if(paths_.size() <= 1)
     return;
 
