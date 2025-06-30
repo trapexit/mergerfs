343c4737195a953ae3cc2febecba9c2c519cda76
Antonio SJ Musumeci <trapexit@spawn.link>
2025-06-29 23:00:43 -0500

    checkpoint

---
+++ src/collectinfo.cpp
@@ -0,0 +1,26 @@
+#include "collectinfo.hpp"
+
+#include "CLI11.hpp"
+
+
+int
+collectinfo::main(int    argc_,
+                  char **argv_)
+{
+  CLI::App app;
+
+  app.description("mergerfs.collectinfo:"
+                  " Collect info for support requests");
+  app.name("USAGE: mergerfs.collectinfo");
+
+  try
+    {
+      app.parse(argc_,argv_);
+    }
+  catch(const CLI::ParseError &e)
+    {
+      return app.exit(e);
+    }
+
+  return 0;
+}
