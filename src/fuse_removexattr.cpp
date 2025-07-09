e6bf973e688ce3ac9cc4fd0da82fe66e41748f73
Antonio SJ Musumeci <trapexit@spawn.link>
2025-07-08 22:22:04 -0500

    fuse_removexattr.cpp

--- src/fuse_removexattr.cpp
+++ src/fuse_removexattr.cpp
@@ -101,7 +101,7 @@ namespace FUSE
   {
     Config::Read cfg;
 
-    if(Config::is_ctrl_xattr(fusepath_,attrname_))
+    if(Config::is_controlfile(fusepath_))
       return -ENOATTR;
 
     if(cfg->xattr.to_int())
