a82b57652ecf98ad81a96227e75677d520c1ec25
Antonio SJ Musumeci <trapexit@spawn.link>
2025-07-17 00:22:22 -0500

    config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -459,7 +459,6 @@ bool
 Config::is_cmd_xattr(const std::string_view &attrname_)
 {
   return nonstd::string::starts_with(attrname_,"user.mergerfs.cmd.");
-  //  return str::startswith(attrname_,"user.mergerfs.cmd.");
 }
 
 std::string
