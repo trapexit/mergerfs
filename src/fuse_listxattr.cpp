350a43fdb698ad645e34ab821edc97255d7e7042
Antonio SJ Musumeci <trapexit@spawn.link>
2025-07-17 23:47:17 -0500

    fuse_listxattr.cpp

--- src/fuse_listxattr.cpp
+++ src/fuse_listxattr.cpp
@@ -78,7 +78,7 @@ _listxattr(const std::vector<Branch*> &branches_,
         return -ERANGE;
       if(rv < 0)
         {
-          if(err != 0)
+          if(!err)
             err = rv;
           continue;
         }
