0913c202b69d510169b4d59869b01c5e9f26f124
Antonio SJ Musumeci <trapexit@spawn.link>
2025-07-17 23:19:13 -0500

    fuse_listxattr.cpp

--- src/fuse_listxattr.cpp
+++ src/fuse_listxattr.cpp
@@ -40,7 +40,7 @@ _listxattr_size(const std::vector<Branch*> &branches_)
 
   for(const auto branch : branches_)
     {
-      fullpath = fs::path::make(obranches[0]->path,fusepath_);
+      fullpath = fs::path::make(branch->path,fusepath_);
 
       rv = fs::llistxattr(branch.path,NULL,0);
       if(rv < 0)
