e0dd79838017789a84be393b06828f1652776efb
Antonio SJ Musumeci <trapexit@spawn.link>
2025-08-27 21:22:22 -0500

    ioprio.hpp

--- src/ioprio.hpp
+++ src/ioprio.hpp
@@ -5,7 +5,7 @@ namespace ioprio
   int get(const int which, const int who);
   int set(const int which, const int who, const int ioprio);
 
-  struct Set
+  struct SetFrom
   {
     SetFrom(const pid_t pid);
   };
