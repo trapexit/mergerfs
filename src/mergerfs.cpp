705eb7a5ea94f2d9f3c8154358266e306840dcba
Antonio SJ Musumeci <trapexit@spawn.link>
2025-10-27 22:23:08 -0500

    mergerfs.cpp

--- src/mergerfs.cpp
+++ src/mergerfs.cpp
@@ -14,6 +14,8 @@
   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
+#include "node.hpp"
+
 #include "mergerfs.hpp"
 #include "mergerfs_fsck.hpp"
 #include "mergerfs_collect_info.hpp"
