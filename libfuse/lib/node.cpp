d88605de722f386248e91724860c1fa5445f648a
Antonio SJ Musumeci <trapexit@spawn.link>
2025-10-27 22:31:33 -0500

    node.cpp

--- libfuse/lib/node.cpp
+++ libfuse/lib/node.cpp
@@ -37,6 +37,7 @@ node_alloc()
       std::lock_guard<std::mutex> guard(g_mutex);
       g_all_stacks.push_back({g_stack,&g_should_gc});
       fmt::print("{} {}",
+
                  gettid(),
                  (void*)g_stack);
       g_initialized = true;
