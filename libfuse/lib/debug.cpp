813151ec733c38e5e39cc78f393ab85a4c046883
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-20 16:19:30 -0600

    debug.cpp

--- libfuse/lib/debug.cpp
+++ libfuse/lib/debug.cpp
@@ -1162,7 +1162,7 @@ debug_fuse_in_header(const struct fuse_in_header *hdr_)
   const void *arg = &hdr_[1];
 
   fmt::print(stderr,
-             "unique=0x%016" PRIx64 ";"
+             "unique=0x{:016x}";"
              " opcode=%s ({});"
              " nodeid={};"
              " uid={};"
