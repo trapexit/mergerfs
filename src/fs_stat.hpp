9ca5809b68945111ee369df09f9698c239b48251
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-21 13:57:16 -0600

    fs_stat.hpp

--- src/fs_stat.hpp
+++ src/fs_stat.hpp
@@ -98,7 +98,7 @@ namespace fs
         return rv;
       }
 
-    auto i = sc<int>(follow_);
+    int i = sc<int>(follow_);
     // auto s = fmt::format("Should never reach this: follow_={}",sc<int>(follow_));
     // DIE("{}",s);
     return -EINVAL;
