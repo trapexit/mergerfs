f22b1903bc15bd776e20e1c601b182fd71f0a894
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-21 13:47:53 -0600

    die.hpp

--- src/die.hpp
+++ src/die.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include "fmt/core.h"
+#include <cstdlib>
 
-[[noreturn]]
-static
-inline
-void
-die(std::string_view msg_)
-{
-  fmt::println(stderr,"FATAL: {}",msg_);
-  std::abort();
-}
+#define DIE(...)                                                        \
+  do {                                                                  \
+    fmt::print(stderr, fmt::emphasis::bold | fg(fmt::color::red),       \
+               "{}:{} in {}: ", __FILE__, __LINE__, __func__);          \
+    fmt::print(stderr, __VA_ARGS__);                                    \
+    fmt::print(stderr, "\n");                                           \
+    std::abort();                                                       \
+  } while(0)
