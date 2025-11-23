0f1c3f7253b0927e6306de9c8d920cc32de93a06
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-23 00:43:34 -0600

    fuse_getattr.cpp

--- src/fuse_getattr.cpp
+++ src/fuse_getattr.cpp
@@ -94,6 +94,7 @@ _getattr(const fs::path  &fusepath_,
                    cfg.symlinkify_timeout);
   if(rv < 0)
     {
+      __builtin_trap();
       fmt::println("getattr: {}\n"
                    " uid: {}\n"
                    " gid: {}\n"
