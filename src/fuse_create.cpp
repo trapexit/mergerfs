7d21df660991cfb1c532bf742e16ab56995377ba
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-23 21:48:49 -0600

    fuse_create.cpp

--- src/fuse_create.cpp
+++ src/fuse_create.cpp
@@ -225,7 +225,8 @@ _create_for_insert_lambda(const fuse_req_ctx_t *ctx_,
 {
   int rv;
   FileInfo *fi;
-  const ugid::Set ugid(ctx_->uid,ctx_->gid);
+  //const ugid::Set ugid(ctx_->uid,ctx_->gid);
+  const ugid::SetRootGuard asdf;
 
   ::_config_to_ffi_flags(cfg,ctx_->pid,ffi_);
   if(cfg.cache_writeback)
