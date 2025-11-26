c95b2ea228ec39e157bed60ef210c6b57f5d132a
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-26 00:36:49 -0600

    policy_ff.cpp

--- src/policy_ff.cpp
+++ src/policy_ff.cpp
@@ -81,7 +81,7 @@ Policy::FF::Search::operator()(const Branches::Ptr  &branches_,
                                const fs::path       &fusepath_,
                                std::vector<Branch*> &output_) const
 {
-  const ugid::SetRootGuard $;
+  const ugid::SetRootGuard $_;
 
   for(auto &branch : *branches_)
     {
