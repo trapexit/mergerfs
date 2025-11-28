b8814bd0af992fc89c4b1d5173c9f5bdfe342402
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-27 21:18:46 -0600

    fuse_chown.cpp

--- src/fuse_chown.cpp
+++ src/fuse_chown.cpp
@@ -100,7 +100,7 @@ FUSE::chown(const fuse_req_ctx_t *ctx_,
             gid_t                 gid_)
 {
   const fs::path  fusepath{fusepath_};
-  const ugid::Set ugid(ctx_);
+  const ugid::SetRootGuard ugid_root_guard;
 
   return ::_chown(cfg.func.chown.policy,
                   cfg.func.getattr.policy,
