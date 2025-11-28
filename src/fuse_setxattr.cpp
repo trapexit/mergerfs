0cb957ac12ac490a34b229de331c38aeb706340b
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-27 22:18:16 -0600

    fuse_setxattr.cpp

--- src/fuse_setxattr.cpp
+++ src/fuse_setxattr.cpp
@@ -194,7 +194,7 @@ _setxattr(const fuse_req_ctx_t *ctx_,
   if(cfg.xattr.to_int())
     return -cfg.xattr.to_int();
 
-  const ugid::Set ugid(ctx_);
+  const ugid::SetRootGuard ugid_root_guard;
 
   return ::_setxattr(cfg.func.setxattr.policy,
                      cfg.func.getxattr.policy,
