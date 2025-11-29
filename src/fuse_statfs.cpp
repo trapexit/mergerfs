cebb98e400c56dd069f5aaf53d5ee535dbaee7cc
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-29 14:00:59 -0600

    fuse_statfs.cpp

--- src/fuse_statfs.cpp
+++ src/fuse_statfs.cpp
@@ -147,7 +147,6 @@ FUSE::statfs(const fuse_req_ctx_t *ctx_,
              struct statvfs       *st_)
 {
   const fs::path  fusepath{fusepath_};
-  const ugid::SetRootGuard ugid_root_guard;
 
   return ::_statfs(cfg.branches,
                    fusepath,
