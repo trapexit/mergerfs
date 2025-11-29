5906c2a39bddb4aee818e15e25318f118e9fa1c7
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-29 12:36:50 -0600

    fs_clonepath.cpp

--- src/fs_clonepath.cpp
+++ src/fs_clonepath.cpp
@@ -119,10 +119,10 @@ fs::clonepath(const fs::path &srcpath_,
 
 static
 int
-_clonedir(const int          srcfd_,
-          const int          dstfd_,
-          const std::string &dirname_,
-          const bool         return_metadata_errors_)
+_clonedir(const int       srcfd_,
+          const int       dstfd_,
+          const fs::path &dirname_,
+          const bool      return_metadata_errors_)
 {
   int rv;
   int srcdirfd;
