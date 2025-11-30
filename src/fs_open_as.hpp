84b764d4b2c9d9e6d624414ffca5b58c17c9b628
Antonio SJ Musumeci <trapexit@spawn.link>
2025-11-29 18:44:13 -0600

    fs_open_as.hpp

--- src/fs_open_as.hpp
+++ src/fs_open_as.hpp
@@ -20,7 +20,27 @@ namespace fs
   }
 }
 #elif defined __FreeBSD__
+namespace fs
+{
+  static
+  inline
+  int
+  open_as(const ugid_t    ugid_,
+          const fs::path &path_,
+          const int       flags_,
+          const mode_t    mode_)
+  {
+    int rv;
 
+    rv = fs::open(path_,flags_,mode_);
+    if(rv < 0)
+      return rv;
+
+    fs::fchown(rv,ugid_.uid,ugid_.gid);
+
+    return rv;
+  }
+}
 #else
 #error "Not Supported!"
 #endif
