471eb21e9b55efe7f2dbdbbd3b6f497b437a0923
Antonio SJ Musumeci <trapexit@spawn.link>
2025-12-15 12:45:13 -0600

    fuse_readdir_cosr.hpp

--- src/fuse_readdir_cosr.hpp
+++ src/fuse_readdir_cosr.hpp
@@ -32,9 +32,12 @@ namespace FUSE
                 unsigned max_queue_depth);
     ~ReadDirCOSR();
 
-    int operator()(const fuse_req_ctx_t   *ctx,
-                   fuse_file_info_t const *ffi,
-                   fuse_dirents_t         *buf);
+    int opendir(const fuse_req_ctx_t *ctx,
+                const char           *fusepath,
+                fuse_file_info_t     *ffi);
+    int readdir(const fuse_req_ctx_t   *ctx,
+                const fuse_file_info_t *ffi,
+                fuse_dirents_t         *buf);
 
   private:
     ThreadPool _tp;
