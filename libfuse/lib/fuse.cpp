613c27d13d02e90632e2a27effa86e1a3e5718f7
Antonio SJ Musumeci <trapexit@spawn.link>
2025-12-15 13:55:15 -0600

    fuse.cpp

--- libfuse/lib/fuse.cpp
+++ libfuse/lib/fuse.cpp
@@ -2086,6 +2086,8 @@ fuse_lib_create(fuse_req_t            *req_,
   err = get_path_name(hdr_->nodeid,name,&fusepath);
   if(!err)
     {
+      fmt::print("create: nodeid={}; name={}; fusepath={};\n",
+                 hdr_->nodeid,name,fusepath);
       err = f.ops.create(&req_->ctx,
                          &fusepath[1],
                          arg->mode,
