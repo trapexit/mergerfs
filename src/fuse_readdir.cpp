cd89458c9f86c7a02d400c1a1eee423cf4ba39e5
Antonio SJ Musumeci <trapexit@spawn.link>
2025-12-15 12:39:31 -0600

    fuse_readdir.cpp

--- src/fuse_readdir.cpp
+++ src/fuse_readdir.cpp
@@ -110,7 +110,7 @@ FUSE::ReadDir::readdir(const fuse_req_ctx_t   *ctx_,
   readdir = std::atomic_load(&_impl);
   assert(readdir);
 
-  rv = (*readdir)(ctx_,ffi_,buf_);
+  rv = readdir->readdir(ctx_,ffi_,buf_);
   if(rv == -ENOENT)
     return ::_handle_ENOENT(ffi_,buf_);
 
