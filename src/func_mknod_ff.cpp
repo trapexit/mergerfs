3c96b138f0f1f543a03cbdca9cc247bcba2f5008
Antonio SJ Musumeci <trapexit@spawn.link>
2025-12-29 18:10:43 -0600

    func_mknod_ff.cpp

--- src/func_mknod_ff.cpp
+++ src/func_mknod_ff.cpp
@@ -40,7 +40,7 @@ Func2::MknodFF::operator()(const ugid_t  &ugid_,
       if(info.spaceavail < branch.minfreespace())
         continue;
 
-      dst_fullpath = dst_branch.path / fusepath_;
+      path = dst_branch.path / fusepath_;
 
       rv = fs::mknod_as(ugid_,fullpath,mode_,dev_);
       if(rv == 0)
