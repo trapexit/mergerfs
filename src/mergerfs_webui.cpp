6f271ae5f5d26fe9961889058843717f8c5196d1
Antonio SJ Musumeci <trapexit@spawn.link>
2026-01-02 00:38:08 -0600

    mergerfs_webui.cpp

--- src/mergerfs_webui.cpp
+++ src/mergerfs_webui.cpp
@@ -57,6 +57,14 @@ _serve_kvs(const httplib::Request &req_,
                    "application/json");
 }
 
+static
+void
+_post_kvs(const httplib::Request &req_,
+          httplib::Response      &res_)
+{
+
+}
+
 int
 mergerfs::webui::main(const int   argc_,
                       char      **argv_)
@@ -71,7 +79,8 @@ mergerfs::webui::main(const int   argc_,
   http_server.Get("/",::_serve_root);
   http_server.Get("/mounts",::_serve_mounts);
   http_server.Get("/kvs",::_serve_kvs);
-  http_server.Post("/kvs", [&](const Request& req, Response& res) {
+  http_server.Post("/kvs", [&](const Request& req, Response& res)
+  {
     try {
       auto j = json::parse(req.body);
       std::string key = j["key"];
