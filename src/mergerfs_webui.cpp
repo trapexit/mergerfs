14d3dc2fd7caff9a94994f522759aadbdb66f75e
Antonio SJ Musumeci <trapexit@spawn.link>
2026-01-03 23:41:34 -0600

    mergerfs_webui.cpp

--- src/mergerfs_webui.cpp
+++ src/mergerfs_webui.cpp
@@ -172,8 +172,7 @@ _get_mounts(const httplib::Request &req_,
     {
       if((not type.empty()) and (mount.type != type))
         continue;
-      // if(mount.type != "fuse.mergerfs")
-      //   continue;
+
       j.push_back(mount.dir);
     }
 
