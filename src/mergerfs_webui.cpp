43424fa5d1d1f36627b0e9aae25b3437f863a66b
Antonio SJ Musumeci <trapexit@spawn.link>
2026-01-05 09:09:16 -0600

    mergerfs_webui.cpp

--- src/mergerfs_webui.cpp
+++ src/mergerfs_webui.cpp
@@ -358,12 +358,12 @@ _post_kvs_key(const httplib::Request &req_,
       if(rv >= 0)
         {
           res_.status = 200;
-          j["result"] = (json)"success";
+          //          j["result"] = (json)"success";
         }
       else
         {
           res_.status = 400;
-          j["result"] = (json)"error";
+          //j["result"] = (json)"error";
           //          j["error"] = ::_generate_error(mount,key,val,rv);
         }
 
