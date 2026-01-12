615ad3443efc46ac2ac0209adf1730d60f5a32a2
Antonio SJ Musumeci <trapexit@spawn.link>
2026-01-12 09:19:35 -0600

    mergerfs_webui.cpp

--- src/mergerfs_webui.cpp
+++ src/mergerfs_webui.cpp
@@ -217,7 +217,7 @@ bool
 _valid_fs_type(const fs::path    &path_,
                const std::string &type_)
 {
-  if(not (str::startswith(path_,"/mnt") or
+  if(not (str::startswith(path_,"/mnt")   or
           str::startswith(path_,"/media") or
           str::startswith(path_,"/srv")))
     return false;
