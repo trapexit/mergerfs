596c42665a8f1f9a66e364d1b1410c851220da2a
Antonio SJ Musumeci <trapexit@spawn.link>
2026-01-12 10:24:55 -0600

    mergerfs_webui.cpp

--- src/mergerfs_webui.cpp
+++ src/mergerfs_webui.cpp
@@ -229,8 +229,6 @@ _valid_fs_type(const fs::path    &path_,
           str::startswith(path_,"/srv")))
     return false;
 
-  auto valid_mount_prefixes = std::initializer_list<std::string_view>
-    {"/mnt","/media","/opt","/tmp","/srv"};
   if(not str::startswith(path_,valid_mount_prefixes))
     return false;
 
