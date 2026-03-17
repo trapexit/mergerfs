7763c7384a634a08fdaf3632e776b93946a21734
Antonio SJ Musumeci <trapexit@spawn.link>
2026-03-17 08:24:05 -0500

    Use fatal::abort() rather than assert in places

--- src/fs_open_fd.cpp
+++ src/fs_open_fd.cpp
@@ -21,15 +21,15 @@
 #if defined(__linux__)
 #include "fmt/core.h"
 #include "fs_openat.hpp"
+#include "fatal.hpp"
 #include "procfs.hpp"
-
-#include <cassert>
 
 int
 fs::open_fd(const int fd_,
             const int flags_)
 {
-  assert(procfs::PROC_SELF_FD_FD > 0);
+  if(procfs::PROC_SELF_FD_FD <= 0)
+    fatal::abort("procfs::PROC_SELF_FD_FD must be > 0");
 
   const int flags = (flags_ & ~O_NOFOLLOW);
 
