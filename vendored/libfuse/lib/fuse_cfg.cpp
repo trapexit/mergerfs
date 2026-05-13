233f99370344ecd6a346d878608f0fc6a57fb048
Antonio SJ Musumeci <trapexit@spawn.link>
2026-05-13 18:30:40 -0500

    Use shared_mutex for fuse log config
    - Guard fuse_cfg_t log file and filepath shared pointers with a shared mutex
    - Replace atomic shared pointer loads and stores with shared and unique locks

--- vendored/libfuse/lib/fuse_cfg.cpp
+++ vendored/libfuse/lib/fuse_cfg.cpp
@@ -1,5 +1,7 @@
 #include "fuse_cfg.hpp"
 
+#include <mutex>
+
 fuse_cfg_t fuse_cfg;
 
 bool
@@ -23,24 +25,32 @@ fuse_cfg_t::valid_umask() const
 std::shared_ptr<FILE>
 fuse_cfg_t::log_file() const
 {
-  return std::atomic_load(&_log_file);
+  std::shared_lock<std::shared_mutex> lk(_log_mutex);
+
+  return _log_file;
 }
 
 void
 fuse_cfg_t::log_file(std::shared_ptr<FILE> f_)
 {
-  std::atomic_store(&_log_file, f_);
+  std::unique_lock<std::shared_mutex> lk(_log_mutex);
+
+  _log_file = std::move(f_);
 }
 
 std::shared_ptr<std::string>
 fuse_cfg_t::log_filepath() const
 {
-  return std::atomic_load(&_log_filepath);
+  std::shared_lock<std::shared_mutex> lk(_log_mutex);
+
+  return _log_filepath;
 }
 
 void
 fuse_cfg_t::log_filepath(const std::string &s_)
 {
-  std::atomic_store(&_log_filepath,
-                    std::make_shared<std::string>(s_));
+  auto filepath = std::make_shared<std::string>(s_);
+  std::unique_lock<std::shared_mutex> lk(_log_mutex);
+
+  _log_filepath = std::move(filepath);
 }
